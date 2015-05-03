9ad411f838d2954dd066aa3b676a3f80650997fb
merou <merousoft@yahoo.fr>
2015-05-03 06:12:17 -0700

    class recette

--- Recette.java
+++ Recette.java
@@ -1,7 +1,15 @@
 /**
- * @author merousoft
- *
+ * @author merousoft.com 
  */
+
+// le package du projet 
+package com.merousoft.master_chef_java8;
+
+/*
+ * ceci est un commentaire 
+ * la classe Recette est la classe de base pour preparer nos recettes 
+ */
+
 public class Recette {
 
 	/**
