af564ad3f60bfde18c058974b8d81c402a29f91e
merou <merousoft@yahoo.fr>
2015-05-07 17:29:41 -0700

    recette

---
+++ Recette1.java
@@ -0,0 +1,24 @@
+package com.merousoft.master_chef_java8;
+
+/**
+ * Ceci est un commentaire javadoc 
+ * @author merousoft
+ */
+
+/*
+ *  ceci est un commentaire sur multiple ligne
+ *  ligne 1 par exemple
+ *  ligne 2 par exemple
+ */
+
+//	ceci est un commentaire sur une seul ligne 
+
+public class Recette1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Bismilah");
+
+	}
+
+}
